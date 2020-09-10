9c615a0231b062a8c615927898963d8e8ff2d10f
Mostyn Bramley-Moore <mostyn@antipode.se>
2020-09-10 23:30:00 +0200

    add missing .

--- .github/workflows/script.bash
+++ .github/workflows/script.bash
@@ -8,7 +8,7 @@ env
 sudo apt-get install jq
 
 remove_label() {
-	url=$(echo $GITHUB_CONTEXT | jq --raw-output event.pull_request._links.issue)/labels/merge-me
+	url=$(echo $GITHUB_CONTEXT | jq --raw-output .event.pull_request._links.issue)/labels/merge-me
 	curl -X DELETE -H "Accept: application/vnd.github.v3+json" "$url"
 }
 
