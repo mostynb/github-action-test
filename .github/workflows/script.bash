81e74a7bf34910eae25b9cf9250be0aa7b5a6395
Mostyn Bramley-Moore <mostyn@antipode.se>
2020-09-10 23:37:06 +0200

    attempt to authenticate

--- .github/workflows/script.bash
+++ .github/workflows/script.bash
@@ -9,7 +9,8 @@ sudo apt-get install jq
 
 remove_label() {
 	url=$(echo $GITHUB_CONTEXT | jq --raw-output .event.pull_request._links.issue.href)/labels/merge-me
-	curl -X DELETE -H "Accept: application/vnd.github.v3+json" "$url"
+	curl -X DELETE -H "Accept: application/vnd.github.v3+json" \
+		--header "authorization: Bearer $GITHUB_TOKEN" "$url"
 }
 
 remove_label
