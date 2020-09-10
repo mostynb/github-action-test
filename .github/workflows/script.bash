7c3d7bf2eb5be3a9197fdc89dc3e774c0b31da53
Mostyn Bramley-Moore <mostyn@antipode.se>
2020-09-10 23:46:24 +0200

    try adding a comment

--- .github/workflows/script.bash
+++ .github/workflows/script.bash
@@ -8,9 +8,22 @@ env
 sudo apt-get install jq
 
 remove_label() {
-	url=$(echo $GITHUB_CONTEXT | jq --raw-output .event.pull_request._links.issue.href)/labels/merge-me
+	local url=$(echo $GITHUB_CONTEXT | jq --raw-output .event.pull_request._links.issue.href)/labels/merge-me
+
 	curl -X DELETE -H "Accept: application/vnd.github.v3+json" \
 		--header "authorization: Bearer $GITHUB_TOKEN" "$url"
 }
 
+add_comment() {
+	local comment="$1"
+	local url=$(echo $GITHUB_CONTEXT | jq --raw-output .event.pull_request._links.comments.href)
+	local data=$(jq --null-input --arg body "$comment" '{ body: $body }')
+
+	curl --request POST --url "$url" \
+		--header "authorization: Bearer $GITHUB_TOKEN" \
+		--header 'content-type: application/json' \
+		--data "$data"
+}
+
+add_comment "testing leaving a comment"
 remove_label
