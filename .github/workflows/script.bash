a57f6e54a1ba3eb99746530389ed457ffc10a65a
Mostyn Bramley-Moore <mostyn@antipode.se>
2020-09-10 23:22:18 +0200

    attempt to remove label

--- .github/workflows/script.bash
+++ .github/workflows/script.bash
@@ -6,3 +6,10 @@ echo "hello from bash script"
 env
 
 sudo apt-get install jq
+
+remove_label() {
+	curl -X DELETE -H "Accept: application/vnd.github.v3+json" \
+		$(jq --raw-output github.event.pull_request._links.issue)/labels/merge-me
+}
+
+remove_label
